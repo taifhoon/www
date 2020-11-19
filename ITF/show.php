63c182d013d7cc54f5c3adda895ac07b27c2deaf
taifhoon <73688884+taifhoon@users.noreply.github.com>
2020-11-19 21:17:12 +0700

    Update show.php

--- ITF/show.php
+++ ITF/show.php
@@ -18,7 +18,7 @@
 					<div class="card-body">
 						<div class="row">
 							<div class="col-6"><h1 class="text-monospace">Data</h1></div>
-							<div class="col-6 text-right"><a href="form.php" class="btn btn-warning btn-sm font-weight-bold">ADD</a></div>
+							<div class="col-6 text-right"><a href="form.php" class="btn btn-success btn-sm font-weight-bold">ADD</a></div>
 						</div>
 						<div class="table-responsive">
 							<?php
