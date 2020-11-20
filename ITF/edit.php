95b9dc7192f36e896d520e2d92840e4bc9c9cd52
taifhoon <73688884+taifhoon@users.noreply.github.com>
2020-11-20 17:56:48 +0700

    Update edit.php

--- ITF/edit.php
+++ ITF/edit.php
@@ -15,7 +15,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Edit | 13th ITF LAB</title>
+	<title>Edit LAB ITF</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
 	<style type="text/css">
@@ -24,7 +24,7 @@
 		}
 	</style>
 </head>
-<body class="bg-light py-5">
+<body class="bg-dark py-5">
 	<div class="container text-dark">
 		<div class="row">
 			<div class="col-12 col-lg-8 offset-lg-2">
