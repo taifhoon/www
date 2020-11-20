a4d95068e13ac4327e13a79c355c93f260e2f395
taifhoon <73688884+taifhoon@users.noreply.github.com>
2020-11-20 18:04:00 +0700

    Update delete.php

--- ITF/delete.php
+++ ITF/delete.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Delete | 13th ITF LAB</title>
+	<title>Delete LAB ITF</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
 	<style type="text/css">
@@ -14,7 +14,7 @@
 		}
 	</style>
 </head>
-<body class="bg-light py-5">
+<body class="bg-dark py-5">
 	<div class="container text-dark">
 		<div class="row">
 			<div class="col-12 col-lg-8 offset-lg-2">
@@ -26,11 +26,11 @@
 								echo "DELETE COMPLETED";
 							}
 							else {
-								echo "FAILED TO DELETE";
+								echo "DELETE FAILED";
 							}
 						?>
 						</h2>
-						<p align="center" class="mt-4 mb-0"><a href="show.php" class="btn btn-sm btn-warning">BACK</a></p>
+						<p align="center" class="mt-4 mb-0"><a href="show.php" class="btn btn-sm btn-success">BACK</a></p>
 					</div>
 				</div>
 			</div>
