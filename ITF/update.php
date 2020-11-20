dee73d9153ecebe66f28914bee226aa1d67771a7
taifhoon <73688884+taifhoon@users.noreply.github.com>
2020-11-20 18:19:28 +0700

    Update update.php

--- ITF/update.php
+++ ITF/update.php
@@ -36,7 +36,7 @@
 							}
 						?>
 						</h2>
-						<p align="center" class="mt-4 mb-0"><a href="show.php" class="btn btn-sm btn-success">BACK</a></p>
+						<p align="center" class="mt-4 mb-0"><a href="show.php" class="btn btn-sm btn-success font-weight-bold">BACK</a></p>
 					</div>
 				</div>
 			</div>
