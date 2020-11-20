742d0fb2ae3a44b0641304d58856dc02e57cb7e5
taifhoon <73688884+taifhoon@users.noreply.github.com>
2020-11-20 17:49:57 +0700

    Update form.php

--- ITF/form.php
+++ ITF/form.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Add | 13th ITF LAB</title>
+	<title>Add LAB ITF</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
 	<style type="text/css">
